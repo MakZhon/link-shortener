10f26ae40e3d394ed83482a2c8f8479788bd0490
MakZhon <abovezer0lol@gmail.com>
2024-05-29 20:58:40 +0200

    init

---
+++ src/main/java/org/zhenmax/linkshortener/LinkShortenerApp.java
@@ -0,0 +1,11 @@
+package org.zhenmax.linkshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerApp.class);
+    }
+}
